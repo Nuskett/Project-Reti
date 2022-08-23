c78df717baa974cf556b1e0098729148e951edff
Nuskett <nuskett@gmail.com>
2022-08-23 10:01:57 +0200

    Create base transfer functions and update gitignore

--- ClientSide/Client.py
+++ ClientSide/Client.py
@@ -8,6 +8,22 @@ FILE_LIST = 'fileList.txt'
 clientSocket = socket(AF_INET,SOCK_DGRAM)
 serverSocketAddress = ('localhost',8080)
 
+#bisogna fare il controllo di hashing e gestione errori
+#aggiungi display status per update del client
+def receiveData():
+    flag, address = clientSocket.recvfrom(BUFFERED_PACKET_SIZE)
+    if (flag.decode('utf8') == 'abort'):
+        print("* File not found\n* Tranfer failed")
+        return
+    filenameToDecode, address = clientSocket.recvfrom(BUFFERED_PACKET_SIZE)
+    filename = filenameToDecode.decode('utf8')
+    receivedFile = open(filename,'wb')
+    packet, address = clientSocket.recvfrom(BUFFERED_PACKET_SIZE)
+    while (packet):
+        receivedFile.write(packet)
+        packet, address = clientSocket.recvfrom(BUFFERED_PACKET_SIZE)
+
+
 while True:
     clientMessage = input("* Type the message to send to the server\n>>")
     command = clientMessage.split()[0]
@@ -16,11 +32,13 @@ while True:
     print(command)
 
     if command == 'exit':
-        sentFlag = clientSocket.sendto(command.encode(),serverSocketAddress)
+        sentFlag = clientSocket.sendto(command.encode('utf8'),serverSocketAddress)
+        print("Closing the client")
         clientSocket.close()
         exit()
     elif command == 'list':
-        sentFlag = clientSocket.sendto(command.encode(),serverSocketAddress)
+        sentFlag = clientSocket.sendto(command.encode('utf8'),serverSocketAddress)
+        receiveData()
     elif command == 'get':
         sentFlag = clientSocket.sendto()
     elif command == 'put':
--- ServerSide/Server.py
+++ ServerSide/Server.py
@@ -1,6 +1,7 @@
 from pydoc import cli
 from socket import *
 import os
+from struct import pack
 import time
 
 BUFFERED_PACKET_SIZE = 2048
@@ -18,6 +19,32 @@ def createFileList():
             fileList.write(file+'\n')
     fileList.close()
 
+def checkFileExistence(filename):
+    if os.path.exists(filename):
+        return True
+    return False
+
+
+#bisogna fare il controllo di hashing
+#aggiungi display status per update del server
+def sendFileToClient(filename,address):
+    #prima controllo se esiste e mando il risultato come messaggio
+    if checkFileExistence(filename):
+        serverSocket.sendto('continue'.encode('utf8'),address)
+    else:
+        serverSocket.sendto('abort'.encode('utf8'),address)
+        return
+    #mando il nome del file per crearlo
+    serverSocket.sendto(filename.encode('utf8'),address)
+    fileToSend = open(filename,'rb')
+    packet = fileToSend.read(BUFFERED_PACKET_SIZE)
+    while (packet):
+        serverSocket.sendto(packet,address)
+        packet = fileToSend.read(BUFFERED_PACKET_SIZE)
+    fileToSend.close()
+
+
+
 while True:
     print("* Waiting for client...")
     data, address = serverSocket.recvfrom(BUFFERED_PACKET_SIZE)
@@ -28,10 +55,12 @@ while True:
 
 
     if command == 'exit':
+        print("Closing the server")
         serverSocket.close
         exit()
     elif command == 'list':
         createFileList()
+        sendFileToClient(FILE_LIST,address)
     elif command == 'get':
         sentFlag = serverSocket.sendto()
     elif command == 'put':
